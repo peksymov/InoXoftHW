be3156b9569f839f246688a483cbe26eb0794eea
serhii <peksymov@gmail.com>
2021-08-23 11:31:50 +0300

    HW2 first commit

---
+++ HomeWork2/database/users.js
@@ -0,0 +1,7 @@
+const users = [
+    {name: "Vasia", age: 23,password: "qwerrty", email: "Vasia@gmail.com"},
+    {name: "Petia", age: 33,password: "qwerrty",email: "Petia@gmail.com"},
+    {name: "Olya", age: 43,password: "qwerrty",email: "Olya@gmail.com"},
+]
+
+module.exports = users
